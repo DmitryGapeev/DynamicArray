d1ffa8cbca27319f0af4ea917afb405437cb3d66
Гапеев Дмитрий Игоревич <Dmitriy.Gapeev@galaktika.by>
2019-01-15 10:43:05 +0300

    Add project files.

---
+++ DynamicArrayProject/DynamicArray.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace AlgorithmsDataStructures
+{
+	public class DynArray<T>
+	{
+		private const int defaultCapacity = 16;
+		public T[] array;
+		public int capacity;
+		public int count;
+
+		public DynArray()
+		{
+			count = 0;
+			MakeArray(16);
+		}
+
+		public void MakeArray(int new_capacity)
+		{
+			if (new_capacity < defaultCapacity)
+				new_capacity = defaultCapacity;
+
+			var newArray = new T[new_capacity];
+
+			if(array != null)
+				Array.Copy(array, newArray, Math.Min(array.Length, newArray.Length));
+
+			array = newArray;
+			capacity = new_capacity;
+		}
+
+		public T GetItem(int index)
+		{
+			// ваш код
+			if (index < 0 || index > count - 1)
+				throw new IndexOutOfRangeException();
+
+			return array[index];
+		}
+
+		public void Append(T itm)
+		{
+			if (count == capacity)
+				MakeArray(capacity * 2);
+
+			count++;
+			array[count - 1] = itm;
+		}
+
+		public void Insert(T itm, int index)
+		{
+			if (index < 0 || index > count)
+				throw new IndexOutOfRangeException();
+
+			if (count == capacity)
+				MakeArray(capacity * 2);
+
+			if (count == index)
+				array[count] = itm;
+			else
+			{
+				Array.Copy(array, index, array, index + 1, count - index);
+				array[index] = itm;
+			}
+
+			count++;
+		}
+
+		public void Remove(int index)
+		{
+			if (index < 0 || index > count - 1)
+				throw new IndexOutOfRangeException();
+
+			if (index < count - 1)
+				Array.Copy(array, index + 1, array, index, count - index);
+
+			array[count - 1] = default(T);
+			count--;
+
+			if (count / (double) capacity < 0.5 && capacity > 16)
+				MakeArray((int) (capacity / 1.5));
+		}
+	}
+}
---
+++ DynamicArrayProject/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TestAppend();
+			TestInsert();
+			TestRemove();
+
+			Console.ReadKey();
+		}
+
+		static void TestAppend()
+		{
+			Console.WriteLine("test append");
+			Console.WriteLine();
+
+			var dynamicArray = new DynArray<int>();
+
+			Console.WriteLine("array count = " + dynamicArray.count);
+			Console.WriteLine("array capacity = " + dynamicArray.capacity);
+
+			for (var i = 0; i < dynamicArray.capacity; i++)
+				dynamicArray.Append( i * i);
+
+			for (var i = 0; i < dynamicArray.count; i++)
+				Console.Write(dynamicArray.GetItem(i) + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("array count = " + dynamicArray.count);
+			Console.WriteLine("array capacity = " + dynamicArray.capacity);
+
+			Console.WriteLine();
+
+			Console.WriteLine("add extra, value 200");
+			dynamicArray.Append(200);
+			Console.WriteLine("array count = " + dynamicArray.count);
+			Console.WriteLine("array capacity = " + dynamicArray.capacity);
+
+			for (var i = 0; i < dynamicArray.count; i++)
+				Console.Write(dynamicArray.GetItem(i) + " ");
+
+			Console.WriteLine();
+			Console.WriteLine(new string('=', 30));
+			
+		}
+
+		static void TestInsert()
+		{
+			Console.WriteLine("test insert");
+			Console.WriteLine();
+
+			var dynamicArray = new DynArray<int>();
+
+			Console.WriteLine("array count = " + dynamicArray.count);
+			Console.WriteLine("array capacity = " + dynamicArray.capacity);
+
+			for (var i = 0; i < dynamicArray.capacity - 3; i++)
+				dynamicArray.Append(i * i);
+
+			for (var i = 0; i < dynamicArray.count; i++)
+				Console.Write(dynamicArray.GetItem(i) + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("insert 1000 into 0");
+			Console.WriteLine("array count before = " + dynamicArray.count);
+			Console.WriteLine("array capacity before = " + dynamicArray.capacity);
+			dynamicArray.Insert(1000, 0);
+			Console.WriteLine("array count after = " + dynamicArray.count);
+			Console.WriteLine("array capacity after = " + dynamicArray.capacity);
+
+			for (var i = 0; i < dynamicArray.count; i++)
+				Console.Write(dynamicArray.GetItem(i) + " ");
+
+			Console.WriteLine(Environment.NewLine);
+			Console.WriteLine("insert 1000 into 7");
+			Console.WriteLine("array count before = " + dynamicArray.count);
+			Console.WriteLine("array capacity before = " + dynamicArray.capacity);
+			dynamicArray.Insert(7000, 7);
+			Console.WriteLine("array count after = " + dynamicArray.count);
+			Console.WriteLine("array capacity after = " + dynamicArray.capacity);
+
+			for (var i = 0; i < dynamicArray.count; i++)
+				Console.Write(dynamicArray.GetItem(i) + " ");
+
+
+			Console.WriteLine(Environment.NewLine);
+			Console.WriteLine("insert 15000 into 15");
+			Console.WriteLine("array count before = " + dynamicArray.count);
+			Console.WriteLine("array capacity before = " + dynamicArray.capacity);
+			dynamicArray.Insert(15000, 15);
+			Console.WriteLine("array count after = " + dynamicArray.count);
+			Console.WriteLine("array capacity after = " + dynamicArray.capacity);
+
+			for (var i = 0; i < dynamicArray.count; i++)
+				Console.Write(dynamicArray.GetItem(i) + " ");
+
+
+			Console.WriteLine(Environment.NewLine);
+			Console.WriteLine("insert 15000 into 15");
+			Console.WriteLine("array count before = " + dynamicArray.count);
+			Console.WriteLine("array capacity before = " + dynamicArray.capacity);
+			dynamicArray.Insert(15000, 15);
+			Console.WriteLine("array count after = " + dynamicArray.count);
+			Console.WriteLine("array capacity after = " + dynamicArray.capacity);
+
+			Console.WriteLine();
+			Console.WriteLine("try to insert -100 into " + (dynamicArray.count +1));
+			try
+			{
+				dynamicArray.Insert(-100, dynamicArray.count + 1);
+			}
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine("exception OutOfRange");
+			}
+
+			Console.WriteLine(new string('=', 30));
+		}
+
+		static void TestRemove()
+		{
+			Console.WriteLine("test count");
+			var dynamicArray = new DynArray<int>();
+			
+			for (var i = 0; i < 30; i++)
+				dynamicArray.Append(i * i);
+
+			for (var i = 0; i < dynamicArray.count; i++)
+				Console.Write(dynamicArray.GetItem(i) + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("array count after = " + dynamicArray.count);
+			Console.WriteLine("array capacity after = " + dynamicArray.capacity);
+
+			Console.WriteLine();
+			Console.WriteLine("Remove items");
+			for (var i = 20; i > 4;i--)
+				dynamicArray.Remove(i);
+
+			Console.WriteLine("array count after = " + dynamicArray.count);
+			Console.WriteLine("array capacity after = " + dynamicArray.capacity);
+			for (var i = 0; i < dynamicArray.count; i++)
+				Console.Write(dynamicArray.GetItem(i) + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("Remove items");
+			for (var i = dynamicArray.count -2; i > 4; i--)
+				dynamicArray.Remove(i);
+
+			Console.WriteLine("array count after = " + dynamicArray.count);
+			Console.WriteLine("array capacity after = " + dynamicArray.capacity);
+			for (var i = 0; i < dynamicArray.count; i++)
+				Console.Write(dynamicArray.GetItem(i) + " ");
+
+			Console.WriteLine();
+			Console.WriteLine("try to delete at " + (dynamicArray.count + 1));
+			try
+			{
+				dynamicArray.Remove(dynamicArray.count + 1);
+			}
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine("exception OutOfRange");
+			}
+		}
+	}
+}
